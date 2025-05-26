e61c392c74bdda4ad8edfeeabe89c21992d727cd
Gennady <shutkov6914@gmail.com>
2025-05-26 18:52:50 +0300

    parse args from cli

--- monitor_app/main.py
+++ monitor_app/main.py
@@ -1,25 +1,17 @@
 import logging
+import argparse
+
 from base import RespiratoryMonitor
 
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s :: %(message)s", level=logging.INFO)
-
-    data_path = r"/home/shutkov/VideoMovement/respmon/"
+    
+    parser = argparse.ArgumentParser()
+    parset.add_argument('-d', '--data_path', type=ascii, default='.')
+    
+    args = parser.parse_args()
+    
+    data_path = args.data_path # path to the folder with video content
 
     rm = RespiratoryMonitor(capture_target=data_path+"small_cat.mp4", save_calibration_image=True, motion_extraction_method="flow")
-
-'''
-    rm = RespiratoryMonitor(capture_target=data_path + "timber.mp4", save_calibration_image=True,
-                            motion_extraction_method="flow")
-    rm = RespiratoryMonitor(capture_target=data_path + "timber.mp4", save_calibration_image=True,
-                            motion_extraction_method="average")
-    rm = RespiratoryMonitor(capture_target=data_path + "timber2.mp4", save_calibration_image=True,
-                            motion_extraction_method="flow")
-    rm = RespiratoryMonitor(capture_target=data_path + "timber2.mp4", save_calibration_image=True,
-                            motion_extraction_method="average")
-    rm = RespiratoryMonitor(capture_target=data_path + "trooper.mp4", save_calibration_image=True,
-                            motion_extraction_method="flow")
-    rm = RespiratoryMonitor(capture_target=data_path + "trooper.mp4", save_calibration_image=True,
-                            motion_extraction_method="average")
-'''
